272cbc49aaecc82100e02d543fced47a414eb00d
JosiahWayne <166679753+JosiahWayne@users.noreply.github.com>
2024-10-09 20:43:28 +0800

    Update Wechat QRCODE

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Through WeChat",
-        "excerpt":" Wechat Official Account     Wechat Interest Group    ","categories": [],
+        "excerpt":"Wechat Official Account     Wechat Interest Group     If the QRCode is not working for you, you can add our assistant: DICtionary by searching for NYUSHDIC_Assistant !      TIPS: You can save the image to album and scan it in WeChat!  ","categories": [],
         "tags": [],
         "url": "/2024/08/23/test.html",
         "teaser": null
